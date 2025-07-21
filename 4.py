68dcc5e40a55b280e60f40e1e0b8f7075cad0c05
poom-56 <37215@bpk.ac.th>
2025-07-21 21:50:55 +0700

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,29 @@
+#ชุดที่ 1
+i = 1
+while i <= 21:
+    print(i, end=", ")
+    i += 2
+
+#ชุดที่ 2
+i = 2
+while i <= 17:
+    print(i, end=", ")
+    i += 3
+
+#ชุดที่ 3
+i = 100
+while i >= -100:
+    print(i, end=", ")
+    i -= 10
+
+#ชุดที่ 4
+i = -30
+while i <= 30:
+    print(i, end=", ")
+    i += 10
+
+#ชุดที่ 5
+i = 15
+while i <= 55:
+    print(i, end=", ")
+    i += 8
