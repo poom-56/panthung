1b6744a83f0afbda681ede36efb9987fa9f465dc
poom-56 <37215@bpk.ac.th>
2025-07-23 22:36:56 +0700

    Add files via upload

---
+++ in4.py
@@ -0,0 +1,29 @@
+print("1. for i in range(1, 101):")
+for i in range(1, 101):
+    print(i, end=' ')
+print("\n--------------------")
+
+print("2. for i in range(7, 77, 7):")
+for i in range(7, 77, 7):
+    print(i, end=' ')
+print("\n--------------------")
+
+print("3. for i in range(20, 1, -2):")
+for i in range(20, 1, -2):
+    print(i, end=' ')
+print("\n--------------------")
+
+print("4. for i in range(2, 18, 3):")
+for i in range(2, 18, 3):
+    print(i, end=' ')
+print("\n--------------------")
+
+print("5. for i in range(55, 0, -11):")
+for i in range(55, 0, -11):
+    print(i, end=' ')
+print("\n--------------------")
+
+print("6. for i in range(1, 0):")
+for i in range(1, 0):
+    print(i, end=' ')
+print("\n--------------------")
